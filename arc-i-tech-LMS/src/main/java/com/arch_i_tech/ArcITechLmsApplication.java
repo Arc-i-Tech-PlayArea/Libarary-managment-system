94942ff633e9787c28e1b19ea086b30571dc12e1
ajstyle <ajdeshmukh241@gmail.com>
2024-07-20 18:55:41 +0530

    create lms-project structure

---
+++ arc-i-tech-LMS/src/main/java/com/arch_i_tech/ArcITechLmsApplication.java
@@ -0,0 +1,13 @@
+package com.arch_i_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcITechLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcITechLmsApplication.class, args);
+	}
+
+}
---
+++ arc-i-tech-LMS/src/test/java/com/arch_i_tech/ArcITechLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arch_i_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcITechLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
